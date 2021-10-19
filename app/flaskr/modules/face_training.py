96e7555b611f68fdd8f621ede63b70285444c599
AlbertHahn <albert.post@mailbox.org>
2021-10-19 16:38:02 +0200

    Added modules and Dockerfile
    Added modules for opencv operations:
    - face recognition
    - face model training
    Added Dockerfile

---
+++ app/flaskr/modules/face_training.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import cv2
+from PIL import Image
+
+class face_training:
+
+    def __init__(self):
+        self
+
+    def recognize(self):
+
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        IMAGE_DIR = os.path.join(BASE_DIR, "images")
+
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")     
+
+        current_id = 0
+        label_ids = {}
+        y_labels = []
+        x_labels = []
+
+        for root, dirs, files in os.walk(IMAGE_DIR):
+            for file in files:
+                if file.endswith("png") or file.endswith("jpg"):
+                    path = os.path.join(root, file)
+                    label = os.path.basename(root).replace(" ", "-").lower()
+                    print(label, path)
