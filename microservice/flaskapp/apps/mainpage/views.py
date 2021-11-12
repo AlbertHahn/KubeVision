0ca3be3882026891b76b4ced387d95a509f211c2
alberthahn <albert.post@mailbox.org>
2021-11-12 15:54:08 +0100

    will be structured differently

--- microservice/flaskapp/apps/mainpage/views.py
+++
@@ -1,9 +0,0 @@
-from . import mainpage, render_template
-
-@mainpage.route("/")
-def home():
-    return render_template('index.html')
-
-@mainpage.app_errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
